f54155f5bed2245b53cb4676ba34d21067e4eee5
David Taylor <davidjamestaylor@gmail.comm>
2017-04-08 15:58:17 -0700

    changed to only be a snort build

--- inspector/src/main/java/Type.java
+++
@@ -1,6 +0,0 @@
-package com.etherfuse.inspector;
-
-public interface Type{
-  public Discovery parse() throws Exception;
-  public void execute(String network) throws Exception;
-}
--- inspector/sweep_cmd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gradle sweep -Pnet="['192.168.1.0/24']"
